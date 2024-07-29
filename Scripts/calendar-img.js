8959db780b3b6d1c53bc53a0006f0797e019ffb7
Thilo Hohl <52962919+thiilo@users.noreply.github.com>
2024-07-29 13:03:55 +0200

    sync calendar-img.js with ioBroker

--- Scripts/calendar-img.js
+++ Scripts/calendar-img.js
@@ -4,22 +4,6 @@ const ical = require('ical');
 
 const { createCanvas, loadImage } = require('canvas');
 
-
-/*******************/
-/*  Datumsabfrage  */
-/*******************/
-
-const today = new Date();
-let day     = today.getDate();
-let month   = addZero(today.getMonth() + 1);
-let year    = today.getFullYear();
-let hour    = today.getHours();
-let minute  = today.getMinutes();
-
-let currentDate = `${year}-${month}-${day}`;
-// currentDate = `2024-02-02`; // Zu Testzwecken Datum im Format 'yyyy-mm-dd' überschreiben
-
-
 /******************/
 /*  Canvas-Setup  */
 /******************/
@@ -29,67 +13,86 @@ const height    = 800;
 const canvas    = createCanvas(width, height);
 const ctx       = canvas.getContext('2d');
 
-const fs_larger = "22px";
-const fs_large  = "20px";
-const fs_reg    = "18px";
-const font      = '"DM Mono"';
-const space     = 30;
+const fs_larger = "32px";
+const fs_large  = "28px";
+const fs_reg    = "24px";
+const font      = 'monospace';
+const space     = 40;
 const bigSpace  = space * 2;
 
-let yPosition   = 50;
+let yPosition;
+let currentDate;
 
-ctx.fillStyle   = 'white';
-ctx.fillRect(0, 0, width, height);
+schedule("*/10 * * * *", function() {
+    /*******************/
+    /*  Datumsabfrage  */
+    /*******************/
 
+    const today = new Date();
+    let day     = today.getDate();
+    let month   = addZero(today.getMonth() + 1);
+    let year    = today.getFullYear();
+    let hour    = today.getHours();
+    let minute  = today.getMinutes();
 
-/*****************/
-/*  Überschrift  */
-/*****************/
+    currentDate = `${year}-${month}-${day}`;
+    // currentDate = `2024-02-02`; // Zu Testzwecken Datum im Format 'yyyy-mm-dd' überschreiben
 
-ctx.textAlign = 'center'
-ctx.fillStyle = 'black';
-ctx.font      = `${fs_larger} ${font}`
-
-ctx.fillText(`Belegungsplan ${day}.${month}.${year}`, 300, yPosition);
-yPosition += space;
-ctx.font = `${fs_large} ${font}`
-ctx.fillText(`Stand ${addZero(hour)}:${addZero(minute)} Uhr`, 300, yPosition);
-yPosition += bigSpace;
-ctx.textAlign = 'left'
-
-
-/**********************/
-/*  Kalender-Quellen  */
-/**********************/
-
-const calendarURLs = [
-    'https://tools.mi.ur.de/davical/public.php/mi-lab/VR4-Labor',
-    'https://tools.mi.ur.de/davical/public.php/mi-lab/VR4-Werkstatt',
-    'https://tools.mi.ur.de/davical/public.php/mi-lab/VR4-Studio'
-    ];
-const names = [
-    'Labor',
-    'Werkstatt',
-    'Studio'
-    ];
+    ctx.fillStyle   = 'white';
+    ctx.fillRect(0, 0, width, height);
 
 
-/******************/
-/*  Zeichen-Loop  */
-/******************/
+    /*****************/
+    /*  Überschrift  */
+    /*****************/
 
-for (let i = 0;  i < calendarURLs.length; i++) {
-    fetchCalendar(calendarURLs[i])
-        .then(data => {
-            const events = readCalendar(data);
-            const organizedEvents = organizeEvents(events);
-            createCalendarImage(organizedEvents, names[i]);
-        })
-        .catch(error => {
-            console.error('Error fetching or processing calendar:', error);
-        }
-    );
-}
+    yPosition = 80;
+
+    ctx.textAlign = 'center'
+    ctx.fillStyle = 'black';
+    ctx.font      = `${fs_larger} ${font}`
+
+    ctx.fillText(`Belegungsplan ${day}.${month}.${year}`, 300, yPosition);
+    yPosition += space;
+    ctx.font = `${fs_large} ${font}`
+    ctx.fillText(`Stand ${addZero(hour)}:${addZero(minute)} Uhr`, 300, yPosition);
+    yPosition += bigSpace;
+    ctx.textAlign = 'left'
+
+
+    /**********************/
+    /*  Kalender-Quellen  */
+    /**********************/
+
+    const calendarURLs = [
+        'https://tools.mi.ur.de/davical/public.php/mi-lab/VR4-Labor',
+        'https://tools.mi.ur.de/davical/public.php/mi-lab/VR4-Werkstatt',
+        'https://tools.mi.ur.de/davical/public.php/mi-lab/VR4-Studio'
+        ];
+    const names = [
+        'Labor',
+        'Werkstatt',
+        'Studio'
+        ];
+
+
+    /******************/
+    /*  Zeichen-Loop  */
+    /******************/
+
+    for (let i = 0;  i < calendarURLs.length; i++) {
+        fetchCalendar(calendarURLs[i])
+            .then(data => {
+                const events = readCalendar(data);
+                const organizedEvents = organizeEvents(events);
+                createCalendarImage(organizedEvents, names[i]);
+            })
+            .catch(error => {
+                console.error('Error fetching or processing calendar:', error);
+            }
+        );
+    }
+});
 
 
 /****************/
@@ -136,6 +139,7 @@ function addZero(month) {
 }
 
 function createCalendarImage(events, name) {
+    ctx.font = `${fs_large} ${font}`;
     ctx.fillText(name, 50, yPosition);
     yPosition += space;
 
@@ -164,6 +168,6 @@ function createCalendarImage(events, name) {
     todayIncluded ? null : ctx.fillText("keine Termine heute.", 50, yPosition); yPosition += bigSpace;
 
     const buffer = canvas.toBuffer('image/png');
-    writeFile('0_userdata.0', '/calendar1.png', buffer, function (error) { }) // ioBroker-compliant
+    writeFile('web', '/calendar1.png', buffer, function (error) { }) // ioBroker-compliant
     // fs.writeFileSync('calendar1.png', buffer); // Zu Testzwecken lokal ausgeben lassen
 }
