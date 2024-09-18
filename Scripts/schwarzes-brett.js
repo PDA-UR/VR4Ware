c0fa23a7538bfc95736ec0f74620ec0e9b56f00f
Leonie <69861021+LeS-crypto@users.noreply.github.com>
2024-09-18 17:18:07 +0200

    Add delete + archive

--- Scripts/schwarzes-brett.js
+++ Scripts/schwarzes-brett.js
@@ -1,44 +1,57 @@
-// Script für Schwarzes Brett (copy of ioBroker script)
-// Creates a HTML-Table (var=htmlID(Public)) from a JSON-object (var=jsonID) to display messages as they get added to another object (var=inputID)
-// updates the JSON object when a checkbox gets clicked to mark a message as "Done" (does not update HTML-Table on click, as of: 02.09.24)
+// Script für Schwarzes Brett
 
 const jsonID = "0_userdata.0.msg.JSON";
+const archiveID = "0_userdata.0.msg.JSON-log";
 const inputID = "0_userdata.0.msg.msgInput";
 const htmlID = "0_userdata.0.msg.HTMLTable";
 const htmlIDPublic = "0_userdata.0.msg.HTMLTablePUBLIC";
 const topicID = "0_userdata.0.msg.msgTopics";
 
-const json = getState(jsonID).val;
-const obj = JSON.parse(json);
-
-// READ json (example)
-for (var msgID in obj) {
-    for (var key in obj[msgID]) {
-        console.log(msgID + ', ' + key + " => " + obj[msgID][key]);
-    }
-}
 
 // A inline script that will update the JSON-object when a message is checked off
 // Only seemed to work this way, as js-events can only be accessed when they are inline or in a script tag (couldn't get that to work)
-const clickScript = 
-`const target = arguments[0].target;
+const clickScript = `
+const target = arguments[0].target;
 const solvedMsgID = arguments[0].target.id;
 const checked = arguments[0].target.checked;
-const oid = '0\_userdata\.0\.msg\.JSON\'
+const oid = '0\_userdata\.0\.msg\.JSON\';
 vis.conn.getStates(oid, (error, states) => {
     const json = states[oid].val;
     let data = JSON.parse(json);
     data[solvedMsgID].resolved = checked;
     vis.setValue('0\_userdata\.0\.msg\.JSON\',JSON.stringify(data));
 });`;
-// This won't generate a new table, so the changes might not visualy persist
-// TODO -> can't access script from inline-event and can't really access widget from inside script (at least don't know how)
+// This updates the JSON-object. A new table is built, when the object changes
+
+const deleteScript = 
+`
+const target = arguments[0].target;
+let boxID = arguments[0].target.id;
+const deletedMsgID = boxID.split('-')[1];
+console.log('del ' + deletedMsgID);
+const oid = '0\_userdata\.0\.msg\.JSON\'
+vis.conn.getStates(oid, (error, states) => {
+    const json = states[oid].val;
+    let data = JSON.parse(json);
+
+    const archive = data[deletedMsgID];
+
+    delete data[deletedMsgID];
+    vis.setValue('0\_userdata\.0\.msg\.JSON', JSON.stringify(data));
+
+    console.log('archive: ' + archive);
+    vis.setValue('0\_userdata\.0\.msg\.JSON-log',JSON.stringify(archive));
+    
+});`
 
 // Parse json to html table (+checkbox) + save in separate object --> to display in vis
 function parseToTable(data, isAdmin, objectID) {
     let adminHeader = "";
         if(isAdmin) {
-            adminHeader = `<th align="left" style="position: sticky; top: 0; background-color: white; color:black;">Done</th>`;
+            adminHeader = `
+                <th align="left" style="position: sticky; top: 0; background-color: white; color:black;">Done</th>
+                <th align="left" style="position: sticky; top: 0; background-color: white; color:black;">Delete</th>
+            `;
     }
 
     const header = 
@@ -70,15 +83,19 @@ function parseToTable(data, isAdmin, objectID) {
         } else if (data[msgID].topic == "tech") {
             topicColor = "#D6B1FC"; // "#373D9A";
         } else if (data[msgID].topic == "study") {
-            topicColor = "#FED7E9";// "#4C058F";
+            topicColor = "#FED7E9"; // "#4C058F";
         }
 
         let checkboxScript = "";
         if(isAdmin) {
             checkboxScript = `
             <td style="border-top: solid 1px #999; background-color:${color}; color:black; padding-left: 5px;" align="center">
-                <input id=${msgID} type="checkbox" name="resolved" ${checked} onclick="${clickScript}">
-            </td>`
+                <input id="${msgID}" type="checkbox" name="resolved" ${checked} onclick="${clickScript}">
+            </td>
+            <td style="border-top: solid 1px #999; background-color:${color}; color:black; padding-left: 5px;" align="center">
+                <input id="D-${msgID}" type="checkbox" name="delete" onclick="${deleteScript}">
+            </td>
+            `
         }
 
         const row = 
@@ -92,14 +109,13 @@ function parseToTable(data, isAdmin, objectID) {
     }
     const table = body + "</table>";
 
-    //console.log("table created");
     setState(objectID, table);
 }
 
 
 // Add msg to JSON on change
 on(inputID, function(obj) {
-    console.log("new msg: " + getState(inputID).val); // Read
+    log("new msg: " + getState(inputID).val); // Read
     const newMsg = getState(inputID).val; 
     const json = getState(jsonID).val;
     let data = JSON.parse(json);
@@ -111,14 +127,13 @@ on(inputID, function(obj) {
     const newTopic = getState(topicID).val;
     // console.log("topic " + newTopic);
 
-    
     data[newID] = {
         topic: newTopic,
         date: newDate,
         msg: newMsg,
         resolved: false, 
     };
-    console.log(data)
+    // console.log(data)
 
     // Ovveride to existing json
     setState(jsonID, JSON.stringify(data));
@@ -128,15 +143,34 @@ on(inputID, function(obj) {
 
 });
 
-// update table, when the Tabs-Widget is clicked ($(...).click is not a funktion)
-/*$('#w000067').click(function () { 
-    console.log("clicked tabs widget");
+// doesn't visually update the widget
+on(jsonID, function() {
     const json = getState(jsonID).val;
     let data = JSON.parse(json);
-    parseToTable(data);
+    parseToTable(data, true, htmlID);
+    parseToTable(data, false, htmlIDPublic)
 });
-*/
 
+// add the deleted msg to the archive-file
+on(archiveID, function() {
+    // TODO: append msg to archived file;
+    const json = getState(archiveID).val;
+    let newLog = JSON.parse(json);
+
+    readFile("0_userdata.0", "schwarzes-brett/archive.json",  function (error, data) { 
+        addToArchive(data, newLog);
+    });
+
+});
+
+function addToArchive(data, newLog) {
+
+    let msgs = JSON.parse(data);
+    const newID = Object.keys(msgs).length;
+
+    msgs[newID] = newLog;
+    writeFile("0_userdata.0", "schwarzes-brett/archive.json", JSON.stringify(msgs), function (error) { log("err: " + error) });
+}
 
 /* Example-JSON
 {
